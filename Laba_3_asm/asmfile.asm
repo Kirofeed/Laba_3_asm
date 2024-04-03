b78274c26d959e6d0dc04c1a9395697138d91f68
drozh <Drozhzhakirill@gmail.com>
2024-04-03 20:53:30 +0300

    feat: done with examples

--- Laba_3_asm/asmfile.asm
+++ Laba_3_asm/asmfile.asm
@@ -1,6 +1,10 @@
 .386
 
 PUBLIC _ProcessArray1@8
+PUBLIC _ProcessArray2
+PUBLIC ProcessArray3 
+PUBLIC ProcessArray4
+PUBLIC @ProcessArray5@8
 
 .model flat
 .data
@@ -61,4 +65,235 @@ pop ebp
 ret 8
 _ProcessArray1@8 endp
 
+
+;**************************************************
+
+
+
+_ProcessArray2 proc
+
+push ebp
+mov ebp, esp
+push esi
+
+mov esi, [ebp + 8]
+mov ebx, [ebp + 12]
+
+mov edx, 0
+
+
+loop1:
+cmp ebx, 0
+je endloop
+
+mov eax, [esi]
+add esi, 4
+dec ebx
+
+mov ecx, ebx
+mov edi, esi
+
+
+in_loop:
+cmp ecx, 0
+je loop1
+
+cmp [edi], eax
+je unique
+add edi, 4
+dec ecx
+jmp in_loop
+
+
+
+
+unique:
+inc edx
+add edi, 4
+dec ecx
+jmp in_loop
+
+
+
+endloop:
+inc edx
+mov eax, edx
+
+pop esi
+mov esp, ebp
+pop ebp
+ret
+_ProcessArray2 endp
+
+
+
+
+
+;**************************************************
+
+
+
+
+ProcessArray3 proc stdcall, arr: dword, sz: dword
+
+
+push esi
+mov esi, arr
+mov ebx, sz
+
+mov edx, 0
+
+
+loop1:
+cmp ebx, 0
+je endloop
+
+mov eax, [esi]
+add esi, 4
+dec ebx
+
+mov ecx, ebx
+mov edi, esi
+
+
+in_loop:
+cmp ecx, 0
+je loop1
+
+cmp [edi], eax
+je unique
+add edi, 4
+dec ecx
+jmp in_loop
+
+
+
+
+unique:
+inc edx
+add edi, 4
+dec ecx
+jmp in_loop
+
+
+
+endloop:
+inc edx
+mov eax, edx
+
+pop esi
+
+ret 8
+ProcessArray3 endp
+
+;**************************************************
+
+ProcessArray4 proc C, arr: dword, sz: dword
+push esi
+mov esi, arr
+mov ebx, sz
+
+mov edx, 0
+
+
+loop1:
+cmp ebx, 0
+je endloop
+
+mov eax, [esi]
+add esi, 4
+dec ebx
+
+mov ecx, ebx
+mov edi, esi
+
+
+in_loop:
+cmp ecx, 0
+je loop1
+
+cmp [edi], eax
+je unique
+add edi, 4
+dec ecx
+jmp in_loop
+
+
+
+
+unique:
+inc edx
+add edi, 4
+dec ecx
+jmp in_loop
+
+
+
+endloop:
+inc edx
+mov eax, edx
+
+pop esi
+
+ret 
+
+ProcessArray4 endp
+
+;**************************************************
+
+
+@ProcessArray5@8 proc
+
+push esi
+
+mov esi, ecx
+mov ebx, edx
+
+mov ecx, 0
+
+mov edx, 0
+
+
+loop1:
+cmp ebx, 0
+je endloop
+
+mov eax, [esi]
+add esi, 4
+dec ebx
+
+mov ecx, ebx
+mov edi, esi
+
+
+in_loop:
+cmp ecx, 0
+je loop1
+
+cmp [edi], eax
+je unique
+add edi, 4
+dec ecx
+jmp in_loop
+
+
+
+
+unique:
+inc edx
+add edi, 4
+dec ecx
+jmp in_loop
+
+
+
+endloop:
+inc edx
+mov eax, edx
+
+pop esi
+
+ret
+
+@ProcessArray5@8 endp
 end
--- Laba_3_asm/main.cpp
+++ Laba_3_asm/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
 extern "C" int __stdcall ProcessArray1(int* arr, int sz);
+extern "C" int __cdecl ProcessArray2(int* arr, int sz);
+extern "C" int __stdcall ProcessArray3(int* arr, int sz);
+extern "C" int __cdecl ProcessArray4(int* arr, int sz);
+extern "C" int __fastcall ProcessArray5(int* arr, int sz);
 
 int main() {
 	int* array = new int[10];
@@ -15,6 +19,11 @@ int main() {
 	array[8] = 5;
 	array[9] = 1;
 
-	std::cout << ProcessArray1(array, 10) << std::endl;
+	std::cout << "proc 1) __stdcall ProcessArray1: " << ProcessArray1(array, 10) << std::endl;
+	std::cout << "proc 2) __cdecl ProcessArray2: " << ProcessArray2(array, 10) << std::endl;
+	std::cout << "proc 3) __stdcall ProcessArray3: " << ProcessArray3(array, 10) << std::endl;
+	std::cout << "proc 4) __stdcall ProcessArray4: " << ProcessArray4(array, 10) << std::endl;
+	std::cout << "proc 5) __fastcall ProcessArray5: " << ProcessArray5(array, 10) << std::endl;
+
 	//system("PAUSE");
 }
